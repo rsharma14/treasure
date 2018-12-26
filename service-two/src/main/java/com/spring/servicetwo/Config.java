35999bdc28c7c1feadbb85b4ad8e7f949c990bf0
sharraje <sharraje@DIN39001212.corp.capgemini.com>
2018-12-26 11:43:09 +0530

    commit

--- service-two/src/main/java/com/spring/servicetwo/Config.java
+++ service-two/src/main/java/com/spring/servicetwo/Config.java
@@ -1,5 +1,6 @@
 package com.spring.servicetwo;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,11 +10,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class Config implements WebMvcConfigurer{
 	
+	@LoadBalanced
 	@Bean
 	RestTemplate geRestTemplate() {
 		return new RestTemplate();
 	}
 	 
+//	 @Bean
+//	 public Docket swaggerApi() {
+//	  return new Docket(DocumentationType.SWAGGER_2)
+//	   .select()
+//	   .apis(RequestHandlerSelectors.basePackage("com.spring.servicetwo.controller"))
+//	   .paths(PathSelectors.any())
+//	   .build()
+//	   .apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
+//	 }
+
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new AdminInterceptor());
 //        registry.addInterceptor(new ThemeInterceptor()).addPathPatterns("/**").excludePathPatterns("/admin/**");
